76396439c9f2a62b6c71376daf7318eeb32c63f9
Nana <naboatng@gmail.com>
2023-12-12 10:18:01 -0500

    code changed to adding tasks

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def add(a,b):
-    return a + b
-print(add(5,3))
+tasks = []
+
+def add_task(task):
+    tasks.append(task)
+    print("Task added:", task)
+
+# Adding tasks
+add_task("Complete assignment")
 
